06872ed1f3fe8f0e16f7fa2abb7913d3f5feec3e
ivan <ivan@budi.com.br>
2022-04-26 09:04:16 -0300

    fixes

--- capitulo2/intercalacao.js
+++ capitulo2/intercalacao.js
@@ -7,10 +7,32 @@ axios.get('intercalacao.htm').then(function (res) {
   data.subtitle = 'Para '
   var A = [2,4,5,7,1,2,3,6]
   p = 0
+  q = A.length/2
   r = A.length
-  q = (r-p)/2
-  console.log(p)
-  console.log(q)
-  console.log(r)
+  merge(A,p,q,r)
   document.getElementById('view').innerHTML = Mustache.render(tpl, data)
+})
+
+
+const merge = ((Array, p, q, r) => {
+  L = Array.slice(p,q)
+  L[L.length] = Math.max(...Array)+1
+  // console.log(L);
+  R = Array.slice(q,r)
+  R[R.length] = Math.max(...Array)+1
+  // console.log(R);
+  i = 0
+  j = 0
+  for (let k = p; k < r; k++) {
+    if (L[i] <= R[j]) {
+      Array[k] = L[i]
+      i++
+    } else {
+      Array[k] = R[j]
+      j++
+    }
+    console.log(Array);
+  }
+  // console.log(Array);
+  return Array
 })
